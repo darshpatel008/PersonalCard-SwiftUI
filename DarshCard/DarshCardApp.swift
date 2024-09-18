b3463f91d4afc9ac9911df837844533676ba116b
darshviroja008 <darshviroja12345@gmail.com>
2024-09-18 08:43:51 +0530

    Initial Commit

---
+++ DarshCard/DarshCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarshCardApp.swift
+//  DarshCard
+//
+//  Created by Darsh viroja  on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct DarshCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
